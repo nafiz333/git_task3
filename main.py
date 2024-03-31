123259438bf3d8417e0e81f390360217ce59c475
Nafiz Haq <nafiz333@hotmail.com>
2024-03-31 16:29:42 +0100

    version1

---
+++ main.py
@@ -0,0 +1,20 @@
+nlp = spacy.load('en_core_web_md')
+
+complaints = [ 'We bought a house in  CA. Our mortgage was handled by a company called ki. Soon after the mortgage was sold to ABC. Shortly after that XYZ took over the mortgage. The other day we got a notice not to send our payment to them but to loi instead. This is all so frustrating and wreaks of the  mortgage nightmare.',
+'I got approved for a loan to buy a house I have submitted everything I need to for them I paid for the inspection and paid good faith check after all of that they said I did not get approved for the loan to cancel my contract because they do not want to wait for the down payments assistant said that the Sellers do not want to wait that long I feel like they are getting over on me I feel that they should have told me that I did not get approved before I spent my money and picked out a house Carrington mortgage in Ohio ',
+'As per the correspondence, I received from : The University  This is to inform you that I have recently pulled my credit report and noticed that there is a collection listing from The University  on my credit report. I WAS never notified of this collection action or that I owed the debt. This letter is to inform you that I would like a verification of the debt and juilo ability to collect this money from me.',
+]
+
+
+
+
+
+print( "--------------------Entity--------------------")  
+for data in complaints:
+    token = nlp(data)
+    for ents in token.ents:
+        print(ents, ents.label_, ents.label)          # label_ gives the code where label gives the numeric code
+
+        
+        
+        
